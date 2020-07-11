e2113bf91933166ee87fd7bde190582f755681de
unknown <jmls9627@gmail.com>
2020-07-11 18:08:47 -0400

    Designing the Landing-Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import { Layout, Header, Navigation, Content } from 'react-mdl';
 import Main from './components/main';
 import { Link } from 'react-router-dom';
 
@@ -9,12 +9,13 @@ class App extends Component {
     return (
       <div className="demo-big-content">
     <Layout>
-        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">MyPortfolio</Link>} scroll>
+        <Header className="header-color" 
+        title={<Link style={{textDecoration: 'none', color: 'white',fontSize:'3em'}} to="/">Portfolio</Link>} scroll>
             <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
+                <Link style={{ color: 'white',fontSize:'20px'}} to="/resume">Resume</Link>
+                <Link style={{ color: 'white',fontSize:'20px'}} to="/aboutme">About Me</Link>
+                <Link style={{ color: 'white',fontSize:'20px'}} to="/projects">Projects</Link>
+                <Link style={{ color: 'white',fontSize:'20px'}} to="/contact">Contact</Link>
             </Navigation>
         </Header>
         <Content>
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,9 +1,36 @@
 import React, {Component} from 'react';
+import {Grid, Cell} from 'react-mdl';
+import foto from '../images/2.jpg'
 
 class Landing extends Component {
 render(){
     return(
-     <div><h1>Landing</h1></div>
+     <div style={{width:'100%', margin:'auto'}}>
+       <Grid className='landing-grid'>
+         <Cell col={12}>
+         <img  src={foto}  alt='foto' className="avatar-img"/>
+          <div className="banner-text">
+           
+            <h1>Software Developer</h1>
+           
+            <br/>
+
+            <p>HTML/CSS | Java | C# | JavaScript | TypeScript | React | Angular | Mongo | SpringMVC </p>
+            
+           <div className="social-links">
+              {/*LinkedIn*/ }
+              <a href="https://www.linkedin.com/in/jose-l%C3%B3pez-179239199/" rel="noopener noreferrer" target="_blank">
+              <i className="fa fa-linkedin-square" aria-hidden='true'/>
+              </a>
+               {/*GitHub*/ }
+               <a href="https://github.com/jmls9627" rel="noopener noreferrer" target="_blank">
+               <i className="fa fa-github-square" aria-hidden='true'/>
+              </a>
+            </div>
+          </div>
+         </Cell>      
+       </Grid>
+     </div>
     )
   }
 }
